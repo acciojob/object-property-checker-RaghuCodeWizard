daa80f2c10e959c389f24bdf08765e3241d54a5c
Raghavendra M <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-09-20 11:34:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  for (let property in sampleObject) {
+    if (property === key) {
+      return true; // Key found, return true
+    }
+  }
+  return false; // Key not found, return false after checking all properties
 }
 
 // Do not change the code below
